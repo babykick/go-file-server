8bee7ea24aa9ccd4bd6b5568324449ede7955212
babykick <babykick@163.com>
2026-01-13 22:15:19 +0800

    更新文件路径并添加.gitignore

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("/Users/stephen/Desktop"))
+	fs := http.FileServer(http.Dir("/mnt/MOBILEDISK1/吉他/卓著谱"))
 
 	http.Handle("/", fs)
 
-	log.Println("文件服务启动：http://0.0.0.0:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("文件服务启动：http://0.0.0.0:8085")
+	log.Fatal(http.ListenAndServe(":8085", nil))
 }
